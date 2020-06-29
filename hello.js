ee1f723f0be3a10df7dd72c37fa636caa1ae9e5d
sumaiyamim <61340272+sumaiyamim612@users.noreply.github.com>
2020-06-29 14:24:10 +0100

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,14 @@
+(function(window){
+
+	var helloSpeaker={};
+
+	helloSpeaker.speak=function(name){
+	console.log(speakWord + " " + name);
+	};
+
+	var speakWord = "Hello"
+	window.helloSpeaker=helloSpeaker;
+
+	var speakWord = "Hello";
+	
+})(window);
